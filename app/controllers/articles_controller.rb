abf7c4e4ca2452bd0a6fc16aa3dfec398575e626
wrblack <wrblack862@gmail.com>
2016-06-26 22:38:05 -0400

    modified articles controller & new & show views. added edit & index views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,11 +17,26 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @articles = Article.all
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :description)
-  end
-
-  def edit
   end
 end
