7a01143daae4cb9511477dda7626484f34f6c72a
wrblack <wrblack862@gmail.com>
2016-07-01 21:44:36 -0400

    Create show action and add gravatar image

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
             length: {minimum: 3, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sensitive: false},
-            length: {minimum: 5, maximum: 20 },
+            length: {minimum: 5, maximum: 80 },
             format: { with: VALID_EMAIL_REGEX }
 
   has_secure_password
